2e1fccc2216a369fbf1f086a9c9493a9e3651420
velasko <h.superpotter@gmail.com>
2020-04-21 09:36:53 +0200

    changed color values to 8bit hex

--- lib/presentation/theme/colors.dart
+++ lib/presentation/theme/colors.dart
@@ -2,44 +2,44 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   /// Application Colors
-  static const Color rudy = const Color(4294837293);
-  static const Color coquelicot = const Color(4294784272);
-  static const Color orange = const Color(4294858752);
-  static const Color candy = const Color(4294839296);
-  static const Color brandeis = const Color(4278218493);
-  static const Color deepSky = const Color(4278240253);
+  static const Color rudy = Color(0xfffe042d);
+  static const Color coquelicot = Color(0xfffd3510);
+  static const Color orange = Color(0xfffe5800);
+  static const Color candy = Color(0xfffe0c00);
+  static const Color brandeis = Color(0xff006efd);
+  static const Color deepSky = Color(0xff00c3fd);
 
   /// Greys
-  static const Color greyDarkest = const Color(4280295717);
-  static const Color greyDark = const Color(4282137920);
-  static const Color grey = const Color(4288454049);
-  static const Color greyLight = const Color(4293059302);
-  static const Color greyLightest = const Color(4294309367);
+  static const Color greyDarkest = Color(0xff202125);
+  static const Color greyDark = Color(0xff3c3d40);
+  static const Color grey = Color(0xff9c9da1);
+  static const Color greyLight = Color(0xffe2e2e6);
+  static const Color greyLightest = Color(0xfff5f5f7);
 
   /// Gradients
-  static const LinearGradient primaryGradient = const LinearGradient(
+  static const LinearGradient primaryGradient = LinearGradient(
     colors: [rudy, orange],
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
   );
-  static const LinearGradient secondaryGradient = const LinearGradient(
+  static const LinearGradient secondaryGradient = LinearGradient(
       colors: [brandeis, deepSky],
       begin: Alignment(-1.0, -2.0), // Alignment.topLeft,
       end: Alignment(1.0, 2.0) // Alignment.bottomRight,
       );
-  static const LinearGradient transparentGradient = const LinearGradient(
+  static const LinearGradient transparentGradient = LinearGradient(
       colors: [Color.fromARGB(0, 0, 0, 0), Color.fromARGB(0, 0, 0, 0)],
       begin: Alignment(-1.0, -2.0), // Alignment.topLeft,
       end: Alignment(1.0, 2.0) // Alignment.bottomRight,
       );
-  static const LinearGradient greyLightestFlatGradient = const LinearGradient(
+  static const LinearGradient greyLightestFlatGradient = LinearGradient(
       colors: [greyLightest, greyLightest],
       begin: Alignment(-1.0, -2.0), // Alignment.topLeft,
       end: Alignment(1.0, 2.0) // Alignment.bottomRight,
       );
 }
 
-ColorScheme colorScheme() => ColorScheme.dark(
+ColorScheme colorScheme() => const ColorScheme.dark(
       primary: AppColors.greyDarkest,
       primaryVariant: Colors.black,
       onPrimary: AppColors.grey,
@@ -55,5 +55,5 @@ int fromHex(String hexString) {
   final buffer = StringBuffer();
   if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
   buffer.write(hexString.replaceFirst('#', ''));
-  return (int.parse(buffer.toString(), radix: 16));
+  return int.parse(buffer.toString(), radix: 16);
 }
