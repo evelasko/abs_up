c81f61ec549261f6790bbf8265104632c3b16e23
velasko <h.superpotter@gmail.com>
2020-05-10 08:28:47 +0200

    failed unity... fallback to flutter

--- unity/abs_up_3d_media/Assets/Scripts/AnimationData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "AnimationData", menuName = "Abs Up/Animation Data")]
-public class AnimationData : ScriptableObject
-{
-    [SerializeField] public List<AnimationClip> animations;
-}
